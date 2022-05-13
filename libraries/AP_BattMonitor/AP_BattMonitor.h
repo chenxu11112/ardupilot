448716135aab316de3c9ab6dd81d27e50118cc28
liaoluweillw <liaoluweillw@163.com>
2022-05-13 19:55:09 +0800

    add tsdh743-fcu

--- libraries/AP_BattMonitor/AP_BattMonitor.h
+++ libraries/AP_BattMonitor/AP_BattMonitor.h
@@ -48,6 +48,7 @@ class AP_BattMonitor_Generator;
 class AP_BattMonitor_INA2XX;
 class AP_BattMonitor_LTC2946;
 class AP_BattMonitor_Torqeedo;
+class AP_BattMonitor_APC;
 
 class AP_BattMonitor
 {
@@ -67,7 +68,7 @@ class AP_BattMonitor
     friend class AP_BattMonitor_LTC2946;
 
     friend class AP_BattMonitor_Torqeedo;
-
+    friend class AP_BattMonitor_APC;
 public:
 
     // battery failsafes must be defined in levels of severity so that vehicles wont fall backwards
@@ -101,6 +102,8 @@ public:
         INA2XX                     = 21,
         LTC2946                    = 22,
         Torqeedo                   = 23,
+        APC                        = 24,
+
     };
 
     FUNCTOR_TYPEDEF(battery_failsafe_handler_fn_t, void, const char *, const int8_t);
