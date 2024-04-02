b3e47e20184f6b232d07c6d86d813782f7381984
Luviewer <liaoluweillw@163.com>
2024-04-02 17:22:58 +0800

    修改名字

--- libraries/AP_Hiwonder/AP_Hiwonder.cpp
+++ libraries/AP_Hiwonder/AP_Hiwonder.cpp
@@ -23,7 +23,7 @@ uint8_t AP_Hiwonder::serial_servo_checksum(const uint8_t buf[])
     return (uint8_t)(~temp);
 }
 
-void AP_Hiwonder::serial_servo_set_position(uint32_t servo_id, int position, uint32_t duration)
+void AP_Hiwonder::set_position(uint32_t servo_id, int position, uint32_t duration)
 {
     Hiwonder_MOVE_WRITE_TypeDef frame;
     frame.header_1 = SERIAL_SERVO_FRAME_HEADER;
