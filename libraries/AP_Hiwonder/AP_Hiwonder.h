ce7a53886ff3cc08e665fa5c513c67186d8d5886
Luviewer <liaoluweillw@163.com>
2024-04-08 21:52:36 +0800

    添加偏差调整

--- libraries/AP_Hiwonder/AP_Hiwonder.h
+++ libraries/AP_Hiwonder/AP_Hiwonder.h
@@ -6,6 +6,8 @@
 
 #define SERIAL_SERVO_FRAME_HEADER 0x55
 #define SERIAL_SERVO_MOVE_TIME_WRITE 1
+#define SERVO_ANGLE_OFFSET_ADJUST 17
+#define SERVO_ANGLE_OFFSET_WRITE 18
 
 enum
 {
@@ -26,6 +28,27 @@ typedef struct
     uint8_t args[4];
     uint8_t crc;
 } Hiwonder_MOVE_WRITE_TypeDef;
+
+typedef struct
+{
+    uint8_t header_1;
+    uint8_t header_2;
+    uint8_t servo_id;
+    uint8_t length;
+    uint8_t command;
+    uint8_t args;
+    uint8_t crc;
+} Hiwonder_OFFSET_ADJUST_TypeDef;
+
+typedef struct
+{
+    uint8_t header_1;
+    uint8_t header_2;
+    uint8_t servo_id;
+    uint8_t length;
+    uint8_t command;
+    uint8_t crc;
+} Hiwonder_WRITE_ADJUST_TypeDef;
 #pragma pack()
 
 class AP_Hiwonder
@@ -35,6 +58,8 @@ public:
 
     void init(void);
     void set_position(uint32_t servo_id, int position, uint32_t duration);
+    void adjust_offset(uint32_t servo_id, int8_t adjust);
+    void write_offset(uint32_t servo_id);
 
     /* Do not allow copies */
     CLASS_NO_COPY(AP_Hiwonder);
